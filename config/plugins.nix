eeed8dac528330a7680d74c3ffe1e5c6647af476
Jaco Kok <kokjaco2@gmail.com>
2024-09-05 18:15:04 +0200

    Basic lsp test added

--- config/plugins.nix
+++ config/plugins.nix
@@ -90,5 +90,177 @@
       };
     };
 
+    # Nix expressions in Neovim
+    nix = {
+      enable = true;
+    };
+
+    # Language server
+    lsp = {
+      enable = true;
+      servers = {
+        # Average webdev LSPs
+        tsserver.enable = true; # TS/JS
+        cssls.enable = true; # CSS
+        tailwindcss.enable = true; # TailwindCSS
+        html.enable = true; # HTML
+        #astro.enable = true; # AstroJS
+        #phpactor.enable = true; # PHP
+        svelte.enable = false; # Svelte
+        #vuels.enable = false; # Vue
+        pyright.enable = true; # Python
+        gopls.enable = true; # Go
+        marksman.enable = true; # Markdown
+        nil-ls.enable = true; # Nix
+        dockerls.enable = true; # Docker
+        bashls.enable = true; # Bash
+        clangd.enable = true; # C/C++
+        csharp-ls.enable = true; # C#
+        yamlls.enable = true; # YAML
+        jsonls.enable = true; # JSON
+
+        lua-ls = {
+          # Lua
+          enable = true;
+          settings.telemetry.enable = false;
+        };
+
+        # Rust
+        rust-analyzer = {
+          enable = true;
+          installRustc = true;
+          installCargo = true;
+        };
+      };
+    };
+
+    cmp-emoji = {
+      enable = true;
+    };
+
+    cmp = {
+      enable = true;
+      settings = {
+        autoEnableSources = true;
+        experimental = { ghost_text = true; };
+        performance = {
+          debounce = 60;
+          fetchingTimeout = 200;
+          maxViewEntries = 30;
+        };
+        snippet = { expand = "luasnip"; };
+        formatting = { fields = [ "kind" "abbr" "menu" ]; };
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "emoji"; }
+          {
+            name = "buffer"; # text within current buffer
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+            keywordLength = 3;
+          }
+          # { name = "copilot"; } # enable/disable copilot
+          {
+            name = "path"; # file system paths
+            keywordLength = 3;
+          }
+          {
+            name = "luasnip"; # snippets
+            keywordLength = 3;
+          }
+        ];
+
+        window = {
+          completion = { border = "solid"; };
+          documentation = { border = "solid"; };
+        };
+
+        mapping = {
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<C-j>" = "cmp.mapping.select_next_item()";
+          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        };
+      };
+    };
+    cmp-nvim-lsp = {
+      enable = true; # LSP
+    };
+    cmp-buffer = {
+      enable = true;
+    };
+    cmp-path = {
+      enable = true; # file system paths
+    };
+    cmp_luasnip = {
+      enable = true; # snippets
+    };
+    cmp-cmdline = {
+      enable = true; # autocomplete for cmdline
+    };
+
+    none-ls = {
+      enable = true;
+      settings = {
+        cmd = [ "bash -c nvim" ];
+        debug = true;
+      };
+      sources = {
+        code_actions = {
+          statix.enable = true;
+          gitsigns.enable = true;
+        };
+        diagnostics = {
+          statix.enable = true;
+          deadnix.enable = true;
+          pylint.enable = true;
+          checkstyle.enable = true;
+        };
+        formatting = {
+          alejandra.enable = true;
+          stylua.enable = true;
+          shfmt.enable = true;
+          nixpkgs_fmt.enable = true;
+          google_java_format.enable = false;
+          prettier = {
+            enable = true;
+            disableTsServerFormatter = true;
+          };
+          black = {
+            enable = true;
+            settings = ''
+              {
+                extra_args = { "--fast" },
+              }
+            '';
+
+          };
+        };
+        completion = {
+          luasnip.enable = true;
+          spell.enable = true;
+        };
+      };
+    };
+
+    # Lazygit
+    lazygit = {
+      enable = true;
+    };
+
+    # Notify
+    notify = {
+      enable = true;
+      backgroundColour = "#1e1e2e";
+      fps = 60;
+      render = "default";
+      timeout = 500;
+      topDown = true;
+    };
+
   };
 }
